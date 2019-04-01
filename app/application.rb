f56db960d385887ec3fe2d5aac3ee4da93bbe6b0
Theo Weller <weller.theodor@gmail.com>
2019-04-01 20:10:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,5 +3,13 @@ def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 
-  
+  @@items = []
+
+  if requ.path.match("/items/<Item Name>")
+
+  else
+    resp.status = 404
+
+
+
 end
