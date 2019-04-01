bebcb469151c5b8a95c84ff183b6832c3ad5269d
Theo Weller <weller.theodor@gmail.com>
2019-04-01 20:15:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,12 +7,15 @@ def call(env)
 @@items << Item.new("Apple", 20)
 @@items << Item.new("Carrot", 5)
 @@items << Item.new("Biscuit", 15)
+
   binding.pry
 
   if requ.path.match("/items/<Item Name>")
 
   else
     resp.status = 404
+
+  end
 
 
 
